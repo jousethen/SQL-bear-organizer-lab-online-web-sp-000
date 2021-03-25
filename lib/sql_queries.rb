7edb8aa1e2df877664f0f2cb4f4155a07dd760e4
Jouse then <jthen93@gmail.com>
2021-03-25 03:45:37 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,17 +1,17 @@
 def selects_all_female_bears_return_name_and_age
-  "Write your SQL query here"
+  "SELECT name, age from bears where gender = 'female'"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "Write your SQL query here"
+  "SELECT name from bears order by name"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "Write your SQL query here"
+  "SELECT name, age from bears where alive = 1 order by age"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "SELECT TOP 1 name, age from bears"
 end
 
 def select_youngest_bear_and_returns_name_and_age
