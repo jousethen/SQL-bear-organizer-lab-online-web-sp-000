662c967b43a89baeb5ac5188fe1703eb917f516c
Jouse then <jthen93@gmail.com>
2021-03-25 03:37:59 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,2 @@
-INSERT INTO bears (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ('Mr. Chocolate', 12, 'Male', "Pink", "Pissy", 1);
