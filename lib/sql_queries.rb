bbd09582e745cd25ce392fe4a35362c6a79b74c4
Jouse then <jthen93@gmail.com>
2021-03-25 03:53:53 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,13 +19,13 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  ""
+  "SELECT color, COUNT(*) from bears"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT COUNT(*) from bears where temperaments = 'goofy'"
+  "SELECT COUNT(*) from bears where temperament = 'goofy'"
 end
 
 def selects_bear_that_killed_Tim
-  "Write your SQL query here"
+  "SELECT * from bears where name is null"
 end
